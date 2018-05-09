53165630cc2cc98125a4710d1e75d70823fe0d54
Quantmgod <quantmgod@gmail.com>
2018-05-09 16:31:41 -0700

    upload python script

---
+++ Quadrick.py
@@ -0,0 +1,28 @@
+import math
+
+def main():
+    print ("Enter the coefficients of:")
+    a = int(input("a: "))
+    b = int(input("b: "))
+    c = int(input("c: "))
+
+    d = b*b-4*a*c
+
+    if d < 0:
+        print ("This equation has no real solution")
+
+    if d >= 0:
+        x1 = (-1 * b + math.sqrt(d))/(2*a)
+        x2 = (-1 * b - math.sqrt(d))/(2*a)
+        if d == 0:
+            print ("This equation has one solutions: ", x1)
+        elif d > 0:
+            print ("This equation has two solutions: ", x1, " and", x2)
+    q = input("Do you want to solve another (Y=Yes/N=No)?: ")
+
+    if ((q == "Y") | (q == "y")):
+        main()
+    else:
+        print("Thanks for playing!")
+        exit()
+main()
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Quadratic
+A simple Python Script that takes 3 input variables and outputs the Quadratic result
